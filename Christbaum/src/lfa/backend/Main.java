4b5906653d00009cae4bae034f1e63b33df186b7
Noel Oliveira <noel.oliveira@swisscom.com>
2018-03-01 11:41:51 +0100

    Christbaum initial commit

---
+++ Christbaum/src/lfa/backend/Main.java
@@ -0,0 +1,38 @@
+package lfa.backend;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, j,y,a,b, stars=0, spaces=0;
+
+        System.out.print("Enter number of branches: ");
+        int branches = sc.nextInt();
+
+        for (i = 0; i <= branches; i++) {
+            spaces = branches - i;
+            for (y = 0; y < spaces; y++){
+                System.out.print(" ");
+            }
+            if (i != 1)
+                stars = i + i - 1;
+            else
+                stars = 1;
+
+            for (j = 0; j < stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(a=0; a<2; a++){
+            for(b=0; b<=branches-2; b++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+    }
+}
+
