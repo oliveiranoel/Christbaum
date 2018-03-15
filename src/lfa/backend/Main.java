3b2f88f587e8b66441d7ba9d7dd4b8b74a85c3d8
Noel Oliveira <noel.oliveira@swisscom.com>
2018-03-15 10:13:41 +0100

    + gitignore

--- src/lfa/backend/Main.java
+++ src/lfa/backend/Main.java
@@ -2,18 +2,22 @@ package lfa.backend;
 
 import java.util.Scanner;
 
+/**
+ * Zeichnet einen Christbaum mit Sternen.
+ * Die Höhe des Baums bestimmt der User, in dem er die Anzahl branches eingibt.
+ */
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i, j,y,a,b, stars=0, spaces=0;
+        int i, j, y, a, b, stars = 0, spaces = 0;
 
         System.out.print("Enter number of branches: ");
         int branches = sc.nextInt();
 
         for (i = 0; i <= branches; i++) {
             spaces = branches - i;
-            for (y = 0; y < spaces; y++){
+            for (y = 0; y < spaces; y++) {
                 System.out.print(" ");
             }
             if (i != 1)
@@ -27,8 +31,8 @@ public class Main {
             System.out.println();
         }
 
-        for(a=0; a<2; a++){
-            for(b=0; b<=branches-2; b++) {
+        for (a = 0; a < 2; a++) {
+            for (b = 0; b <= branches - 2; b++) {
                 System.out.print(" ");
             }
             System.out.println("*");
